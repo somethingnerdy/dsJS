539ca03181665bf10c5f088c5f8e04b8cd8efd0b
somethingnerdy <angelrodriguez403339@gmail.com>
2016-03-15 20:50:51 -0700

    refactoring stack and queue class

--- src/queue.js
+++ src/queue.js
@@ -1,50 +1,82 @@
-if (typeof define !== 'function') { var define = require('../node_modules/amdefine')(module); }
-
-define(function () {
+/**
+ * @class Queue
+ * @classdesc A Queue is a First-In First-Out (FIFO) data structure. Data is inserted at the end
+ *            and are removed from the front. Queues are used to store data in the order in
+ *            which they occur.
+ * @constructor
+ */
+dsJS.Queue = function () {
     /**
-     * @module Queue
-     * @description First-In First-Out (FIFO)
+     * @property enqueue
+     * @description Pushes an element to the front of the queue
+     * @param {String | Number} element Element to store in the queue
      */
-
-    /**
-     * @name Queue
-     * @constructor
-     * @property listSize {Number} Number of elements in the list
-     * @property position {Number} Current position in the list
-     * @property dataStore {Array} Array to store a list of elements
-     */
-    function Queue () {
-        'use strict';
-        this.dataStore = [];
-    }
-
-    Queue.prototype = {
-        enqueue = function (element) {
-            this.dataStore.push(element);
-        },
-        dequeue = function () {
-            if (this.dataStore.length > 0) {
-                return this.dataStore.shift();
-            }
-        },
-        front = function () {
-            return this.dataStore[0];
-        },
-        back = function () {
-            return this.dataStore[this.dataStore.length - 1];
-        },
-        toString = function () {
-            var string = '';
-            for (var i = 0, len = this.dataStore.length; i < len; i++) {
-                string += this.dataStore[i] + '\n';
-            }
-            return string;
-        },
-        empty = function () {
-            if (this.dataStore.length === 0) {
-                return true;
-            }
-            return false;
-        },
+    var enqueue = function (element) {
+        this.dataStore.push(element);
     };
-});
+    /**
+     * @property dequeue
+     * @description
+     * @returns {T}
+     */
+    var dequeue = function () {
+        if (this.dataStore.length > 0) {
+            return this.dataStore.shift();
+        }
+    };
+    /**
+     * @property front
+     * @description
+     * @returns {*}
+     */
+    var front = function () {
+        return this.dataStore[0];
+    };
+    /**
+     * @property back
+     * @description
+     * @returns {*}
+     */
+    var back = function () {
+        return this.dataStore[this.dataStore.length - 1];
+    };
+    /**
+     * @property toString
+     * @description
+     * @returns {string}
+     */
+    var toString = function () {
+        var string = '';
+        for (var i = 0, len = this.dataStore.length; i < len; i++) {
+            string += this.dataStore[i] + '\n';
+        }
+        return string;
+    };
+    /**
+     * @property isEmpty
+     * @description Checks to see if the queue doesn't contain elements.
+     * @returns {boolean} True, if there are elements in the stack. False, if the
+     *                    queue is empty
+     */
+    var isEmpty = function () {
+        return this.dataStore.length === 0;
+    };
+    /**
+     * @property length
+     * @returns {*|j|i|o}
+     */
+    var length = function () {
+        return this.dataStore.length;
+    };
+    /** @lends Queue */
+    return {
+        dataStore : [],
+        enqueue : enqueue,
+        dequeue : dequeue,
+        front : front,
+        back : back,
+        toString : toString,
+        isEmpty : isEmpty,
+        length: length
+    }
+};
--- src/stack.js
+++ src/stack.js
@@ -1,9 +1,66 @@
 /**
  * @class Stack
- * @classdesc A Stack is a First-In Last-Out (FIFO) data structure.
+ * @classdesc A Stack is a First-In Last-Out (FILO) data structure.
  * @constructor
  */
 dsJS.Stack = function () {
+    /**
+     * @property push
+     * @description Pushes an element onto the top of the stack (dataStore).
+     *              This function is equivalent to JS's array.push()
+     * @param {String | Number} element Value to push onto the stack.
+     */
+    var push = function (element) {
+        this.dataStore[this.top++] = element;
+    };
+    /**
+     * @property pop
+     * @description Removes the element at the top of the stack
+     * @returns {*} The top element of the stack. If the stack/dataStore is empty
+     *              it will return undefined.
+     */
+    var pop = function () {
+        return this.dataStore[--this.top];
+    };
+    /**
+     * @property peek
+     * @description Returns the element at the top of the stack without popping it.
+     * @returns {*} The element at the top of the stack. Undefined if the stack is empty
+     */
+    var peek = function () {
+        return this.dataStore[this.top - 1];
+    };
+    /**
+     * @property length
+     * @description
+     * @returns {number}
+     */
+    var length = function () {
+        return this.top;
+    };
+    /**
+     * @property clear
+     * @description Clears the stack. Sets the top and the length of dataStore to 0
+     */
+    var clear = function () {
+        this.top = 0;
+        this.dataStore.length = 0;
+    };
+    /**
+     * @property isEmpty
+     * @description Checks to see if the stack doesnt contain elements.
+     * @returns {boolean} True, if there are elements in the stack. False, if the
+     *                    stack is empty
+     */
+    var isEmpty = function () {
+        return this.top <= 0;
+    };
+    var show = function () {
+        if (window.console) {
+            console.log(this.dataStore);
+        }
+    };
+
     /** @lends Stack */
     return {
         /**
@@ -18,55 +75,12 @@ dsJS.Stack = function () {
          *              in the Stack class.
          */
         top: 0,
-        /**
-         * @property push
-         * @description Pushes an element onto the top of the stack (dataStore).
-         *              This function is equivalent to JS's array.push()
-         * @param {String | Number} element Value to push onto the stack.
-         */
-        push : function (element) {
-            this.dataStore[this.top++] = element;
-        },
-        /**
-         * @property pop
-         * @description Removes the element at the top of the stack
-         * @returns {*} The top element of the stack. If the stack/dataStore is empty
-         *              it will return undefined.
-         */
-        pop : function () {
-            return this.dataStore[--this.top];
-        },
-        /**
-         * @property peek
-         * @description
-         * @returns {*}
-         */
-        peek : function () {
-            return this.dataStore[this.top - 1];
-        },
-        /**
-         * @property length
-         * @description
-         * @returns {number}
-         */
-        length : function () {
-            return this.top;
-        },
-        /**
-         * @property clear
-         * @description
-         */
-        clear : function () {
-            this.top = 0;
-            this.dataStore.length = 0;
-        },
-        /**
-         * @property isEmpty
-         * @description
-         * @returns {boolean}
-         */
-        isEmpty : function () {
-            return this.top <= 0;
-        }
+        push : push,
+        pop : pop,
+        peek : peek,
+        length : length,
+        clear : clear,
+        isEmpty : isEmpty,
+        show : show
     }
 };
