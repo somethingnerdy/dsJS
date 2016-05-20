d139ced56413d5301fff415fd568b53e07d7ec47
somethingnerdy <angelrodriguez403339@gmail.com>
2016-05-20 16:12:58 -0700

    Adding test cases for dictionary and adding Linked List

--- src/linked-list.js
+++ src/linked-list.js
@@ -1,58 +1,189 @@
-
+dsJS.LinkedList = function () {
     /**
-     * @module Linked List
-     * @description
-     */
-
-    function Node (element) {
-        'use strict';
-        this.element = element;
-        this.next = null;
-    }
-    /**
-     * @name Linked List
+     * TODO: make a utility object to inherit this
+     * @param element
+     * @returns {{element: *, next: null}}
      * @constructor
-     * @property listSize {Number} Number of elements in the list
-     * @property position {Number} Current position in the list
-     * @property dataStore {Array} Array to store a list of elements
      */
-    function LinkedList () {
-        'use strict';
-        this.head = new Node('head');
-    }
+    var Node = function (element) {
+        return {
+            element: element,
+            next: null
+        };
+    };
+    var length = 0;
+    var head = null;
+    /**
+     *
+     * @returns {*}
+     */
+    var getHead = function () {
+        return head;
+    };
+    /**
+     * This adds a new item to the end of the list
+     * @param element {String|Number} The element to store
+     */
+    var append = function (element) {
+        var node = Node(element);
+        var current;
 
-    LinkedList.prototype = {
-        find: function (item) {
-            var currentNode = this.head;
-            while (currentNode.element !== item) {
-                currentNode = currentNode.next;
+        if (head === null) {
+            head = node;
+        } else {
+            current = head;
+
+            while (current.next) {
+                current = current.next;
             }
-            return currentNode;
-        },
-        insert: function (element, item) {
-            var newNode = new Node(element);
-            var current = this.find(item);
-            newNode.next = current.next; //set the new node to point to whatever the current was pointing to.
-            current.next = newNode; //change the current to point to the newly inserted node.
-        },
-        findPrevious: function (item) {
-            var currentNode = this.head;
-            while ((currentNode.next !== null) && (currentNode.next.element !== item)) {
-                currentNode = currentNode.next;
+
+            current.next = node;
+        }
+        length++;
+    };
+    /**
+     * Inserts a new item at a specified position in the list
+     * @param position {number} The position to insert the element
+     * @param element {number|string}
+     * @returns {boolean}
+     */
+    var insert = function (position, element) {
+        if (position >= 0 && position <= length) {
+            var node = Node(element);
+            var current = head;
+            var previous;
+            var index = 0;
+
+            if (position === 0) {
+                node.next = current;
+                head = node;
+            } else {
+                while (index < position) {
+                    previous = current;
+                    current = current.next;
+                    index++;
+                }
+                node.next = current;
+                previous.next = node;
             }
-            return currentNode;
-        },
-        remove: function (item) {
-            var prevNode = this.findPrevious(item);
-            if (prevNode.next !== null) {
-                prevNode.next = prevNode.next.next;
-            }
-        },
-        display: function () {
-            var currNode = this.head;
-            while (!(currNode.next == null)) {
-                console.log(currNode.next.element);
-                currNode = currNode.next;
-            }
+            length++;
+            return true;
+        } else {
+            return false;
         }
     };
+    /**
+     * This removes an item from the list.
+     * @param element
+     * @returns {null}
+     */
+    var remove = function (element) {
+        var index = indexOf(element);
+        return removeAt(index);
+    };
+    /**
+     * This returns the index of the element in the list. If the element is not in the list, it returns -1.
+     * @param element
+     * @returns {number}
+     */
+    var indexOf = function (element) {
+        var current = head;
+        var index = -1;
+
+        while (current) {
+            if (element === current.element) {
+                return index;
+            }
+            index++;
+            current = current.next;
+        }
+        return -1;
+    };
+    /**
+     * Removes an item from a specified position in the list
+     * @param position {Number} The position of the to be removed element
+     * @returns {null|}
+     */
+    var removeAt = function (position) {
+        if (position >= 0 && position < length) {
+            var current = head;
+            var previous;
+            var index = 0;
+
+            if (position === 0) {
+                head = current.next;
+            } else {
+                while (index < position) {
+                    previous = current;
+                    current = current.next;
+                    index++;
+                }
+
+                previous.next = current.next;
+            }
+            length--;
+            return current.element;
+        } else {
+            return null;
+        }
+    };
+    /**
+     * This returns true if the linked list does not contain any elements and false if the size of the linked list is bigger than 0.
+     * @returns {boolean}
+     */
+    var isEmpty = function () {
+        return length === 0;
+    };
+    /**
+     * This returns how many elements the linked list contains.
+     * @returns {number}
+     */
+    var size = function () {
+        return length;
+    };
+    /**
+     * As the list uses a Node class as an item, we need to overwrite the default toString method inherited from the JavaScript object to output only the element values
+     * @returns {string}
+     */
+    var toString = function () {
+        var current = head;
+        var string = '';
+
+        while (current) {
+            string += current.element + ' -> ';
+            current = current.next;
+        }
+        return string;
+    };
+    var reverse = function () {
+        var current = head;
+        var previous = null;
+        var temp;
+        if (head === null || head.next === null) {
+            return;
+        }
+
+        while (current) {
+            temp = current.next;
+            current.next = previous;
+            previous = current;
+            current = temp;
+        }
+        head = previous;
+        return previous;
+    };
+    var reverseBetween = function(m, n) {};
+    var hasCycle = function () {};
+    return {
+        getHead: getHead,
+        append: append,
+        insert: insert,
+        remove: remove,
+        indexOf: indexOf,
+        removeAt: removeAt,
+        reverse: reverse,
+        isEmpty: isEmpty,
+        size: size,
+        toString: toString
+    };
+};
--- test/dictionary.js
+++ test/dictionary.js
@@ -30,13 +30,32 @@ describe('Dictionary Library', function () {
         keys.should.containEql('SF');
         done();
     });
-   /* it('should return all of the proper values or the dictionary', function (done) {
-
+    it('should return all of the proper values or the dictionary', function (done) {
+        map.set('SF', 'CA');
+        map.set('NY', 'NY');
+        map.set('ATL', 'GA');
+        map.size().should.eql(3);
+        map.values().should.containEql('CA', 'NY', 'GA');
+        map.values().length.should.eql(3);
+        done();
     });
     it('should retrieve all of the keys and then clear', function (done) {
-
+        map.set('A', 'B');
+        map.set('B', 'C');
+        map.set('C', 'D');
+        map.size().should.eql(3);
+        map.keys().should.containEql('A', 'B', 'C');
+        map.keys().length.should.eql(3);
+        map.clear();
+        map.size().should.eql(0);
+        map.keys().length.should.eql(0);
+        done();
     });
     it('should override a the value of an existing key-value pair', function (done) {
-
-    });*/
+        map.set('Name', 'John');
+        map.get('Name').should.eql('John');
+        map.set('Name', 'Peter');
+        map.get('Name').should.eql('Peter');
+        done();
+    });
 });
